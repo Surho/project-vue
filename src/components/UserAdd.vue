2d07934db797e371b8c95e150783d4deaa242ea3
Surho <surho007@hotmail.com>
2019-10-24 00:10:40 +0300

    work in progress

--- src/components/UserAdd.vue
+++ src/components/UserAdd.vue
@@ -1,33 +1,109 @@
 <template>
-  <div>
-    <div v-for="fieldName in fieldNames" :key="fieldName">
-      <div class="input-group mb-3">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="basic-addon1">{{ fieldName }}</span>
-        </div>
-        <input
-          type="text"
-          class="form-control"
-          placeholder=""
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-        />
+  <div class="form-add">
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">First name</span>
       </div>
+      <input type="text" class="form-control" placeholder="" v-model="user.firstName" />
     </div>
-    <button type="button" class="btn btn-primary">Add user</button>
+
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Last Name</span>
+      </div>
+      <input type="text" class="form-control" placeholder="" v-model="user.lastName" />
+    </div>
+
+    <div class="form-group form-check">
+      <input type="checkbox" class="form-check-input" id="isActive" v-model="user.isActive" />
+      <label class="form-check-label" for="isActive">Is active</label>
+    </div>
+
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Balance</span>
+      </div>
+      <input type="text" class="form-control" placeholder="" v-model="user.balance" />
+    </div>
+
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Email</span>
+      </div>
+      <input type="text" class="form-control" placeholder="" v-model="user.email" />
+    </div>
+
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Phone</span>
+      </div>
+      <input type="text" class="form-control" placeholder="" v-model="user.phone" />
+    </div>
+
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Age</span>
+      </div>
+      <input type="text" class="form-control" placeholder="" v-model="user.age" />
+    </div>
+
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Access level</span>
+      </div>
+      <input type="text" class="form-control" placeholder="" v-model="user.accessLevel" />
+    </div>
+
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Company</span>
+      </div>
+      <input type="text" class="form-control" placeholder="" v-model="user.company" />
+    </div>
+
+    <div class="input-group mb-3 input-add">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Image</span>
+      </div>
+      <input type="text" class="form-control" placeholder="" />
+    </div>
+
+    <button type="button" class="btn btn-primary" @click="addUser">Add user</button>
   </div>
 </template>
 <script>
-// import NprogressContainer from 'vue-nprogress/src/NprogressContainer'
+import axios from 'axios'
 
 export default {
   data() {
     return {
-      fieldNames: ['firstName', 'lastName', 'age', 'company', 'email', 'phone']
+      fieldNames: ['firstName', 'lastName', 'age', 'company', 'email', 'phone'],
+      user: {
+        id: 0,
+        isActive: '',
+        balance: '',
+        picture: '',
+        age: '',
+        firstName: '',
+        lastName: '',
+        company: '',
+        email: '',
+        phone: '',
+        about: '',
+        accessLevel: '',
+        registered: Date.now()
+      }
     }
   },
-  components: {
-    // NprogressContainer
+  methods: {
+    addUser() {
+      axios.post(`http://localhost:3000/users/`, this.user)
+    }
   }
 }
 </script>
+<style>
+.form-add {
+  text-align: left;
+}
+</style>
--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>User details <span class="badge badge-secondary">Add user</span></h1>
+    <h1><span class="badge badge-secondary">Add user</span></h1>
     <user-add></user-add>
   </div>
 </template>
